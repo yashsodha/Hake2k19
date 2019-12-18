6c1dac388ab675a30e1d6c5ba129e7d15ccdfaf9
yash sodha <yash@digitaltrons.com>
2019-12-18 15:30:19 +0530

    ChatListView

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -7,110 +8,122 @@
  */
 
 import React, { Component } from 'react';
-import {
-  SafeAreaView,
-  View,
-  Text,
-  StatusBar,
-  Button
-} from 'react-native';
-import Dialogflow from "react-native-dialogflow"
-
-import Dialogflow_V2 from "react-native-dialogflow"
+import { SafeAreaView, View, Text, StatusBar, Button, ScrollView, FlatList } from 'react-native';
+import Dialogflow from 'react-native-dialogflow';
+import Dialogflow_V2 from 'react-native-dialogflow';
 import { TextInput } from 'react-native-gesture-handler';
 
-
-
+let chatArray = [];
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: "hello",
-      myMessage: "",
-      botMessage: ""
-    }
+      message: null,
+      myMessage: null,
+      botMessage: null,
+      chatData: [],
+    };
     Dialogflow.setConfiguration(
-      "cec9711b9662437686a454a19496065d", Dialogflow.LANG_ENGLISH
+      'cec9711b9662437686a454a19496065d',
+      Dialogflow.LANG_ENGLISH,
     );
   }
 
+  handleResponse = res => {
+    chatArray.push({
+      send: res.result.resolvedQuery,
+      receive: res.result.fulfillment.speech,
+    });
+    this.setState({
+      chatData: chatArray,
+    });
+  };
+
+  renderChats = ({ item }) => {
+    console.log('---->item', item);
+    return (
+      <ScrollView>
+        <View style={{ marginTop: 10, marginRight: 3 }}>
+          <Text
+            style={{
+              fontSize: 20,
+              alignSelf: 'flex-end',
+              borderRadius: 14,
+              padding: item.send ? 7 : 0,
+              backgroundColor: '#22AAEB',
+              color: 'white',
+            }}>
+            {item.send}
+          </Text>
+        </View>
+
+        {item.receive && (
+          <View style={{ marginTop: 10, marginLeft: 3 }}>
+            <Text
+              style={{
+                fontSize: 20,
+                borderRadius: 14,
+                padding: item.receive ? 7 : 0,
+                backgroundColor: 'gray',
+                color: 'white',
+              }}>
+              {item.receive}
+            </Text>
+          </View>
+        )}
+      </ScrollView>
+    );
+  };
 
   render() {
+    const { chatData } = this.state;
     return (
       <>
         <StatusBar barStyle="dark-content" />
         <SafeAreaView>
-          <View
-            style={{ height: "100%" }}>
-            <Text style={{ fontSize: 20, alignSelf: "flex-end", borderWidth: 1, padding: 20 }}>{this.state.myMessage}</Text>
-            <Text style={{ fontSize: 20 }}>{this.state.botMessage}</Text>
+          <View style={{ height: '100%' }}>
+            <FlatList
+              data={chatData}
+              extraData={chatData}
+              renderItem={this.renderChats}
+              keyExtractor={index => console.log('---->index', index)}
+            />
 
-
-
-            {/* <Button
-              title="start"
-              onPress={() => {
-                Dialogflow_V2.onListeningStarted(result => {
-                  console.log(result);
-                })
-              }}
-            /> */}
-            {/* 
-            <View style={{ marginTop: 20 }}
-            >
-              <Button
-                title="Stop"
-                onPress={() => {
-                  Dialogflow_V2.onListeningFinished(result => {
-                    console.log("stop", result);
-                  });
-                }}
-              />
-
-
-              <View style={{ marginTop: 20 }}>
-
-                
-
-
-              </View>
-
-            </View> */}
-
-            <View style={{ flexDirection: "row", alignSelf: "flex-end", position: "absolute", bottom: 15 }}>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignSelf: 'flex-end',
+                position: 'absolute',
+                bottom: 15,
+              }}>
               <TextInput
-                style={{ borderWidth: 2, width: "80%" }}
+                style={{ borderWidth: 2, width: '80%' }}
                 value={this.state.message}
-                onChangeText={message => this.setState({
-                  message
-                })}
+                onChangeText={message =>
+                  this.setState({
+                    message,
+                  })
+                }
               />
-
-              <View style={{ margin: 5, marginTop: 10, width: "15%" }}>
+              <View style={{ margin: 5, marginTop: 10, width: '15%' }}>
                 <Button
                   title="Send"
                   onPress={() => {
                     Dialogflow.requestQuery(
                       this.state.message,
-                      res =>
-                        this.setState({
-                          myMessage: res.result.resolvedQuery,
-                          botMessage: res.result.fulfillment.speech
-                        }),
-                      error => console.log(error));
-                  }
-                  }
+                      this.handleResponse,
+                      error => console.log(error),
+                    );
+                  }}
                 />
               </View>
             </View>
-
-
           </View>
         </SafeAreaView>
       </>
     );
-  };
+  }
 }
 
 export default App;
